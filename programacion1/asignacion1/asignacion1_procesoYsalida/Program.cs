7431c03efc30f6aaba618b6e6e1c7654b35b7737
marcellofdz <marcellofernandez98@gmail.com>
2020-11-04 23:20:33 -0400

    cambiando de ubicación los archivos

---
+++ programacion1/asignacion1/asignacion1_procesoYsalida/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace asignacion1_procesoYsalida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mostrar nombre completo por pantalla
+            Console.WriteLine("Marcello Fernandez");
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+        //2. Mostrar por pantalla de manera separadas sus nombres y apellidos
+
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Marcello");
+            Console.WriteLine("\nFernandez");
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+
+    }
+}
