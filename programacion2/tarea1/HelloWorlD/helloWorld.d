1d92141334b506a512e1e3cc25e5e91d651e8953
marcellofdz <marcellofernandez98@gmail.com>
2020-11-16 22:55:48 -0400

    Añadiendo asignaciones nuevas

---
+++ programacion2/tarea1/HelloWorlD/helloWorld.d
@@ -0,0 +1,13 @@
+import std.stdio;
+
+//D Lenguage
+/*Programa que te imprime un saludo personalizado*/
+
+void main(string[] args)
+{
+	string nombre;
+	write ("Dime tu nombre: ");
+	readf("%s\n", &nombre);
+	writeln("Hola ", nombre);
+
+}
