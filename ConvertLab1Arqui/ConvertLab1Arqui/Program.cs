92e16b7b489e68fafa4eb060bc0308209796e763
Marcello Fernandez <marcellofernandez98@gmail.com>
2021-06-06 00:42:32 -0400

    Convertir ASCII a entero y viceversa

---
+++ ConvertLab1Arqui/ConvertLab1Arqui/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertLab1Arqui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string salir = "no";
+
+            while (salir.ToLower() == "no")
+            {
+                Console.WriteLine("Convertido de ASCII a Entero - Lab Arquitectura del computador");
+                Console.WriteLine(" ================= SELECCIONE UNA OPCION ==================");
+                Console.WriteLine("1. ASCII a Entero");
+                Console.WriteLine("2. Entero a ASCII");
+                Console.Write("Opción [1-2]: ");
+                string opcion = Console.ReadLine();
+
+                Console.WriteLine();
+
+                switch (opcion)
+                {
+                    case "1":
+                        try
+                        {
+                            Console.WriteLine("ASCII a Entero");
+                            Console.Write("Ingrese un caracter: ");
+                            char letra = Convert.ToChar(Console.ReadLine());
+                            int num = (int)letra;
+                            Console.WriteLine("El resultado de {0} en ASCII es {1}", letra,num);
+                            Console.ReadLine();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                        }
+                        break;
+                    case "2":
+                        try
+                        {
+                            Console.WriteLine("Entero a ASCII");
+                            Console.Write("Ingrese un numero: ");
+                            int num2 = int.Parse(Console.ReadLine());
+                            char letra2 = (char)num2;
+                            Console.WriteLine("El resultado de {1} en entero es {0}", letra2, num2);
+                            Console.ReadLine();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                        }
+                        break;
+                }
+
+                Console.Write("Desea salir? Si/no : ");
+                salir = Console.ReadLine();
+            }
+
+            //Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConvertLab1Arqui/ConvertSNLab1Arqui/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertSNLab1Arqui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string salir = "no";
+
+            Console.WriteLine("================ CONVERTIDOR DE BINARIO A DECIMAL  - Lab Arquitectura del computador ================");
+            Console.WriteLine();
+            while (salir.ToLower() == "no")
+            {
+                Console.WriteLine();
+                Console.Write("Ingrese el numero binario: ");
+                int Binario = int.Parse(Console.ReadLine());
+                int vdecimal = 0;
+
+                int base1 = 1;
+
+                while (Binario > 0)
+                {
+                    int reminder = Binario % 10;
+                    Binario = Binario / 10;
+                    vdecimal += reminder * base1;
+                    base1 = base1 * 2;
+                }
+                Console.Write($"Valor en decimal es: {vdecimal} ");
+                Console.ReadKey();
+                Console.WriteLine();
+
+                Console.Write("Desea salir? Si/no : ");
+                salir = Console.ReadLine();
+            }
+
+        }
+    }
+}
